653f03d95156c7a57e819dc3a3eeeeb2b58ec040
Asraful Islam <iasraful321@gmail.com>
2023-01-30 20:32:43 +0600

    redux updated

---
+++ src/redux/actionCreators/productAction.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import { ADD_TO_CART } from "../actionTypes/actionTypes";
+
+export const addToCart = (product) => {
+  return {
+    type: ADD_TO_CART,
+    payload: product,
+  };
+};
